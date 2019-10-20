e79a2287b7c3d87d7b23cd8a7bcae2434c6650f7
Hemang Vyas <vyashemang563@gmail.com>
2019-10-20 13:40:00 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -2,6 +2,6 @@ import smtplib
 connection = smtplib.SMTP('smtp.gmail.com',587)
 connection.ehlo() #to connect server with gmail
 connection.starttls() # to start encryption
-connection.login('#sender mail#', 'sender pwd')
+connection.login('#sender mail#', 'sender pwd') # enter email id and password.
 connection.sendmail('#sender mail#','#reciver mail#','Subject: Write Your Subject \n\n Write here to send')
 connection.quit()
