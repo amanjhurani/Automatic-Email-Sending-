f547b8f2d177761e4ac9320f44d55bed262a032e
amanopensource0 <govindsolanki.ice@gmail.com>
2021-10-23 16:57:54 +0530

    fixed rolling output

--- Static.py
+++ Static.py
@@ -6,15 +6,4 @@ connection.login('#sender mail#', 'sender pwd') # enter email id and password.
 connection.sendmail('#sender mail#','#reciver mail#','Subject: Write Your Subject \n\n Write here to send')
 connection.quit()
 
-def set_smtp_domain(serviseprovider):
-    if serviseprovider == 'gmail':
-        return 'smtp.gmail.com'
-    elif serviseprovider == 'outlook' or serviseprovider == 'hotmail':
-        return 'smtp-mail.outlook.com'
-    elif serviseprovider == 'yahoo':
-        return 'smtp-mail.yahoo.com'
-print("Welcome You to send email through this program")
-print("Please Enter your Email And Password : ")
-e_mail , serviceProvider = get_mail()
-password = input("Password : ")
 
